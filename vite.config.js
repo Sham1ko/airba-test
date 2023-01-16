2a7d16ee8b9a6d33348ec3b21c2c93ff14c82510
Shamshyrak Zholdasbek <shamshyrak.zholdasbek@gmail.com>
2023-01-16 16:00:08 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "airba-test",
+  base: "/airba-test/",
   plugins: [vue()],
   resolve: {
     alias: {
